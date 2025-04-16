24949e060d76358e5b8ec9bf520a14c169f66285
MonzonPabloS <monzonpablo1989@gmail.com>
2025-04-16 16:57:51 -0300

    de todo

--- main.js
+++ main.js
@@ -4,17 +4,42 @@ const mas = document.getElementById("icono-agregar");
 
 const lista = document.getElementById("check");
 
-const agregarTareas = (tarea) => {
+const hecho = 'fas fa-circle';
+
+const pendiente = 'fa-circle';
+
+const tachado = 'tachado';
+
+const agregarTareas = (tarea, check) => {
+    const estado = check ? hecho : pendiente;
+    const tachar = check ? tachado : "";
 const elemento = `
 <li>
-                <i class="fas fa-circle co" id="check" data-="check"></i>
-                <p class="tarea" id="tarea">${tarea}</p>
-                <i class="fas fa-trash de" id="borrar" data-="borrar"></i>
+                <i class="far ${estado}" Data="check"></i>
+                <p class="tarea ${tachado}" id="tarea">${tarea}</p>
+                <i class="fas fa-trash de" id="borrar" Data="borrar"></i>
                 
             </li>`
 lista.insertAdjacentHTML("beforeend", elemento); 
 
 };
+
+const TareaRealizada = (element) => {
+element.classList.toggle(hecho);
+element.classList.toggle(pendiente);
+element.parentNode.querySelector ('.tarea').classList.toggle(tachado)
+};
+
+const tareaEliminada = (element) => {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+} 
+
+
+const cambiarEstilos = () => {
+    const link = document.getElementById("estilos"); 
+    link.href = link.href.includes("style.css") ? "style2.css" : "style.css";  
+}
+
 
 
 //llamas función
@@ -41,7 +66,14 @@ document.addEventListener('keyup', (e) => {
     }
 })
 
-const cambiarEstilos = () => {
-    const link = document.getElementById("estilos"); 
-    link.href = link.href.includes("style.css") ? "style2.css" : "style.css";  
+lista.addEventListener('click', function(event) {
+    const element = event.target;
+    const elementData = element.atributes.data.value;
+
+if (elementData == 'check') {
+    tareaRealizada (element)
 }
+else if (elementData =='borrar') {
+    tareaEliminada (element)
+}
+})
